d836d9e588f1797af566d99066f0fb622188bce8
onlymint <ngc4nh@gmail.com>
2024-09-23 09:15:10 +0700

    Them file

---
+++ src/main/java/com/example/cnweb_nhom5/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.example.cnweb_nhom5.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    Page<Product> findAll(Pageable page);
+
+    Page<Product> findAll(Specification<Product> spec, Pageable page);
+
+
