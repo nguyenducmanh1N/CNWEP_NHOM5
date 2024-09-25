b43b8caae864082e34a83c61e4c7efb55452b36d
VanThuyHien <vh140823@gmail.com>
2024-09-25 19:09:40 +0700

    Create VoucherService.java

---
+++ src/main/java/com/example/cnweb_nhom5/service/VoucherService.java
@@ -0,0 +1,8 @@
+package com.example.cnweb_nhom5.service;
+
+import org.springframework.stereotype.Service;
+import vn.hoidanit.laptopshop.domain.Voucher;
+import vn.hoidanit.laptopshop.repository.VoucherRepository;
+
+import java.util.List;
+import java.util.Optional;
