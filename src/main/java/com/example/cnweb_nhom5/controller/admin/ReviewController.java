6e0d8896c454259795f5f93f3348069d1dddfb91
VanThuyHien <vh140823@gmail.com>
2024-09-25 19:07:45 +0700

    Update and rename CartController.java to ReviewController.java

---
+++ src/main/java/com/example/cnweb_nhom5/controller/admin/ReviewController.java
@@ -0,0 +1,11 @@
+package com.example.cnweb_nhom5.controller.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
