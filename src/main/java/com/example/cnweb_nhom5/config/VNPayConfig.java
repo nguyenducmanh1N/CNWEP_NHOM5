098843ad5a817e718487fad0ab5e5c81072520da
nguyenducmanh1N <manhn@gmail.com>
2024-10-02 23:26:10 +0700

    "fix method createPayment"

--- src/main/java/com/example/cnweb_nhom5/config/VNPayConfig.java
+++ src/main/java/com/example/cnweb_nhom5/config/VNPayConfig.java
@@ -18,26 +18,27 @@ public class VNPayConfig {
 
     public static String vnp_Version = "2.1.0";
     public static String vnp_Command = "pay";
-    //mk 123456Mm
-     public static String md5(String message) {
-         String digest = null;
-         try {
-             MessageDigest md = MessageDigest.getInstance("MD5");
-             byte[] hash = md.digest(message.getBytes("UTF-8"));
-             StringBuilder sb = new StringBuilder(2 * hash.length);
-             for (byte b : hash) {
-                 sb.append(String.format("%02x", b & 0xff));
-             }
-             digest = sb.toString();
-         } catch (UnsupportedEncodingException ex) {
-             digest = "";
-         } catch (NoSuchAlgorithmException ex) {
-             digest = "";
-         }
-         return digest;
-     }
 
-     public static String Sha256(String message) {
+    // mk 123456Mm
+    public static String md5(String message) {
+        String digest = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(message.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            digest = sb.toString();
+        } catch (UnsupportedEncodingException ex) {
+            digest = "";
+        } catch (NoSuchAlgorithmException ex) {
+            digest = "";
+        }
+        return digest;
+    }
+
+    public static String Sha256(String message) {
         String digest = null;
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -55,7 +56,7 @@ public class VNPayConfig {
         return digest;
     }
 
-    //Util for VNPAY
+    // Util for VNPAY
     public static String hashAllFields(Map fields) {
         List fieldNames = new ArrayList(fields.keySet());
         Collections.sort(fieldNames);
@@ -73,9 +74,9 @@ public class VNPayConfig {
                 sb.append("&");
             }
         }
-        return hmacSHA512(secretKey,sb.toString());
+        return hmacSHA512(secretKey, sb.toString());
     }
-    
+
     public static String hmacSHA512(final String key, final String data) {
         try {
 
@@ -97,5 +98,28 @@ public class VNPayConfig {
         } catch (Exception ex) {
             return "";
         }
+    }
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ipAdress;
+        try {
+            ipAdress = request.getHeader("X-FORWARDED-FOR");
+            if (ipAdress == null) {
+                ipAdress = request.getRemoteAddr();
+            }
+        } catch (Exception e) {
+            ipAdress = "Invalid IP:" + e.getMessage();
+        }
+        return ipAdress;
+    }
+
+    public static String getRandomNumber(int len) {
+        Random rnd = new Random();
+        String chars = "0123456789";
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+        return sb.toString();
     }
 }
--- src/main/java/com/example/cnweb_nhom5/controller/vnpay/PaymentController.java
+++ src/main/java/com/example/cnweb_nhom5/controller/vnpay/PaymentController.java
@@ -1,52 +1,96 @@
-package com.example.cnweb_nhom5.controller.vnpay;
+package  com.example.cnweb_nhom5.controller.vnpay;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TimeZone;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cnweb_nhom5.config.VNPayConfig;
+
+import com.example.cnweb_nhom5.domain.Payment;
+
+import com.example.cnweb_nhom5.domain.User;
+import com.example.cnweb_nhom5.domain.dto.PaymentInfoVNPAYDTO;
+import com.example.cnweb_nhom5.domain.dto.PaymentRestDTO;
+import com.example.cnweb_nhom5.domain.dto.TransactionStatusDTO;
+import com.example.cnweb_nhom5.service.PaymentService;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 @RequestMapping("/api/payment")
 public class PaymentController {
+
+    
+    private final PaymentService paymentService;
+
+    public PaymentController( PaymentService paymentService) {
+
+        this.paymentService = paymentService;
+    }
+
     @GetMapping("/create_payment")
     // ResponseEntity<?>
     public void createPayment(
-        @RequestParam double totalPrice,
-        HttpServletResponse response,
-        HttpServletRequest request
-            
-            ) throws IOException  {
+            @RequestParam double totalPrice,
+            HttpServletResponse response,
+            HttpServletRequest request
+            // ,
+            // @RequestParam("receiverName") String receiverName,
+            // @RequestParam("receiverAddress") String receiverAddress,
+            // @RequestParam("receiverPhone") String receiverPhone,
+            // @RequestParam("paymentId") Long paymentId
+            ) throws IOException {
 
         System.out.println("Total Price: " + totalPrice);
-        //long amount = Integer.parseInt(req.getParameter("amount"))*100;
+        // String orderType = req.getParameter("other");
+        // long amount = Integer.parseInt(req.getParameter("amount"))*100;
+        // String bankCode = req.getParameter("bankCode");
+        // public static String vnp_Version = "2.1.0";
+        // public static String vnp_Command = "pay";
+        long amount = (long) (totalPrice * 100);
+        String vnp_TxnRef = VNPayConfig.getRandomNumber(8);
+        // String vnp_IpAddr = VNPayConfig.getIpAddress(req);
         String vnp_TmnCode = VNPayConfig.vnp_TmnCode;
+
+        // Map<String, Object> payment_info = new HashMap<>();
+        // payment_info.put("payment_id", paymentId);
         ObjectMapper mapper = new ObjectMapper();
+
+        // PaymentInfoVNPAYDTO payment_info = new PaymentInfoVNPAYDTO(paymentId, receiverName, receiverAddress,
+        //         receiverPhone);
+
+        //String payment_info_str = mapper.writeValueAsString(payment_info);
+
         Map<String, String> vnp_Params = new HashMap<>();
-        
+
         vnp_Params.put("vnp_Version", VNPayConfig.vnp_Version);
         vnp_Params.put("vnp_Command", VNPayConfig.vnp_Command);
         vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
-        //vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
         vnp_Params.put("vnp_CurrCode", "VND");
         vnp_Params.put("vnp_BankCode", "NCB");
 
-        
-        
+        // if (bankCode != null && !bankCode.isEmpty()) {
+        // vnp_Params.put("vnp_BankCode", bankCode);
+        // }
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
         //vnp_Params.put("vnp_OrderInfo", payment_info_str);
         vnp_Params.put("vnp_Locale", "vn");
         // vnp_Params.put("vnp_OrderType", orderType);
@@ -73,15 +117,54 @@ public class PaymentController {
         String vnp_ExpireDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 
-        // List fieldNames = new ArrayList(vnp_Params.keySet());
-        // Collections.sort(fieldNames);
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
         StringBuilder hashData = new StringBuilder();
         StringBuilder query = new StringBuilder();
-        
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                // Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                // Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
         String queryUrl = query.toString();
         String vnp_SecureHash = VNPayConfig.hmacSHA512(VNPayConfig.secretKey, hashData.toString());
         queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
         String paymentUrl = VNPayConfig.vnp_PayUrl + "?" + queryUrl;
+
+        // PaymentRestDTO paymentRestDTO = new PaymentRestDTO();
+        // paymentRestDTO.setStatus("OKE");
+        // paymentRestDTO.setMessage("successfully");
+        // paymentRestDTO.setUrl(paymentUrl);
+
+        // com.google.gson.JsonObject job = new JsonObject();
+        // job.addProperty("code", "00");
+        // job.addProperty("message", "success");
+        // job.addProperty("data", paymentUrl);
+        // Gson gson = new Gson();
+        // resp.getWriter().write(gson.toJson(job));
+
+        // return ResponseEntity.status(HttpStatus.OK).body(paymentRestDTO);
+        // + "&receiverName=" + receiverName + "&receiverAddress=" + receiverAddress
+        // +"&receiverPhone="+ receiverPhone+"&paymentId="+ paymentId
+        // System.out.println("Receiver Name: " + receiverName);
+        // System.out.println("Receiver Address: " + receiverAddress);
+        // System.out.println("Receiver Phone: " + receiverPhone);
+        // System.out.println("Payment ID: " + paymentId);
         response.sendRedirect(paymentUrl);
     }
+
 }
