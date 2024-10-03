652d8ce4166b7ca548dc21242299ff883fab9038
onlymint <ngc4nh@gmail.com>
2024-10-03 21:04:04 +0700

    update

--- src/main/java/com/example/cnweb_nhom5/service/ProductService.java
+++ src/main/java/com/example/cnweb_nhom5/service/ProductService.java
@@ -235,58 +235,6 @@ public class ProductService {
         }
     }
 
-    // public void handlePlaceOrder(
-    //         User user, HttpSession session,
-    //         String receiverName, String receiverAddress, String receiverPhone) {
-
-    //     // step 1: get cart by user
-    //     Cart cart = this.cartRepository.findByUser(user);
-    //     if (cart != null) {
-    //         List<CartDetail> cartDetails = cart.getCartDetails();
-
-    //         if (cartDetails != null) {
-
-    //             // create order
-    //             Order order = new Order();
-    //             order.setUser(user);
-    //             order.setReceiverName(receiverName);
-    //             order.setReceiverAddress(receiverAddress);
-    //             order.setReceiverPhone(receiverPhone);
-    //             order.setStatus("PENDING");
-
-    //             double sum = 0;
-    //             for (CartDetail cd : cartDetails) {
-    //                 sum += cd.getPrice();
-    //             }
-    //             order.setTotalPrice(sum);
-    //             order = this.orderRepository.save(order);
-
-    //             // create orderDetail
-
-    //             for (CartDetail cd : cartDetails) {
-    //                 OrderDetail orderDetail = new OrderDetail();
-    //                 orderDetail.setOrder(order);
-    //                 orderDetail.setProduct(cd.getProduct());
-    //                 orderDetail.setPrice(cd.getPrice());
-    //                 orderDetail.setQuantity(cd.getQuantity());
-
-    //                 this.orderDetailRepository.save(orderDetail);
-    //             }
-
-    //             // step 2: delete cart_detail and cart
-    //             for (CartDetail cd : cartDetails) {
-    //                 this.cartDetailRepository.deleteById(cd.getId());
-    //             }
-
-    //             this.cartRepository.deleteById(cart.getId());
-
-    //             // step 3 : update session
-    //             session.setAttribute("sum", 0);
-    //         }
-    //     }
-
-    // }
-
     public void handlePlaceOrder(
             User user, HttpSession session,
             String receiverName, String receiverAddress, String receiverPhone
