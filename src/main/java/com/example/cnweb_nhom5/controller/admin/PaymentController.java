26dd9f18d36c3c33adcb70768bcd263f41948efe
NgocAnh-21 <ntngocanh0503@gmail.com>
2024-10-10 08:45:27 +0700

    PaymentController4

--- src/main/java/com/example/cnweb_nhom5/controller/admin/PaymentController.java
+++ src/main/java/com/example/cnweb_nhom5/controller/admin/PaymentController.java
@@ -1,11 +1,15 @@
 package com.example.cnweb_nhom5.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -43,3 +47,22 @@ public class PaymentController {
         String vnp_TxnRef = VNPayConfig.getRandomNumber(8);
         // String vnp_IpAddr = VNPayConfig.getIpAddress(req);
         String vnp_TmnCode = VNPayConfig.vnp_TmnCode;
+        
+        // Map<String, Object> payment_info = new HashMap<>();
+        // payment_info.put("payment_id", paymentId);
+        ObjectMapper mapper = new ObjectMapper();
+
+        PaymentInfoVNPAYDTO payment_info = new PaymentInfoVNPAYDTO(paymentId, receiverName,receiverAddress,receiverPhone);
+        
+        String payment_info_str = mapper.writeValueAsString(payment_info);
+
+
+        Map<String, String> vnp_Params = new HashMap<>();
+         
+        vnp_Params.put("vnp_Version", VNPayConfig.vnp_Version);
+        vnp_Params.put("vnp_Command", VNPayConfig.vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+        vnp_Params.put("vnp_BankCode", "NCB");
+        
