e5ea4117ec5307f587a2e816579c75972c4a8ef4
NgocAnh-21 <ntngocanh0503@gmail.com>
2024-10-10 08:43:53 +0700

    PaymentController3

--- src/main/java/com/example/cnweb_nhom5/controller/admin/PaymentController.java
+++ src/main/java/com/example/cnweb_nhom5/controller/admin/PaymentController.java
@@ -1,7 +1,14 @@
 package com.example.cnweb_nhom5.controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/api/payment")
@@ -13,3 +20,26 @@ public class PaymentController {
         this.productService = productService;
         this.paymentService = paymentService;
     }
+    
+    @GetMapping("/create_payment")
+    // ResponseEntity<?>
+    public void createPayment(
+        @RequestParam double totalPrice,
+        HttpServletResponse response,
+        HttpServletRequest request,
+            @RequestParam("receiverName") String receiverName,
+            @RequestParam("receiverAddress") String receiverAddress,
+            @RequestParam("receiverPhone") String receiverPhone,
+            @RequestParam("paymentId") Long paymentId
+            ) throws IOException  {
+
+        System.out.println("Total Price: " + totalPrice);
+        // String orderType = req.getParameter("other");
+        // long amount = Integer.parseInt(req.getParameter("amount"))*100;
+        // String bankCode = req.getParameter("bankCode");
+        // public static String vnp_Version = "2.1.0";
+        // public static String vnp_Command = "pay";
+        long amount = (long) (totalPrice * 100);
+        String vnp_TxnRef = VNPayConfig.getRandomNumber(8);
+        // String vnp_IpAddr = VNPayConfig.getIpAddress(req);
+        String vnp_TmnCode = VNPayConfig.vnp_TmnCode;
