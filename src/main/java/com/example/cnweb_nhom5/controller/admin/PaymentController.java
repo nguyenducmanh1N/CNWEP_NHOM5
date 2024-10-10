273d769410d5b1d05ee4d4c75aa69967c952ed22
NgocAnh-21 <ntngocanh0503@gmail.com>
2024-10-10 08:49:19 +0700

    PaymentController5

--- src/main/java/com/example/cnweb_nhom5/controller/admin/PaymentController.java
+++ src/main/java/com/example/cnweb_nhom5/controller/admin/PaymentController.java
@@ -1,14 +1,27 @@
 package com.example.cnweb_nhom5.controller;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.cnweb_nhom5.config.VNPayConfig;
+import com.example.cnweb_nhom5.domain.dto.PaymentInfoVNPAYDTO;
+import com.example.cnweb_nhom5.service.PaymentService;
+import com.example.cnweb_nhom5.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -65,4 +78,51 @@ public class PaymentController {
         vnp_Params.put("vnp_Amount", String.valueOf(amount));
         vnp_Params.put("vnp_CurrCode", "VND");
         vnp_Params.put("vnp_BankCode", "NCB");
-        
+       
+       
+        // if (bankCode != null && !bankCode.isEmpty()) {
+        // vnp_Params.put("vnp_BankCode", bankCode);
+        // }
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", payment_info_str);
+        vnp_Params.put("vnp_Locale", "vn");
+        // vnp_Params.put("vnp_OrderType", orderType);
+        String orderType = "other";
+        vnp_Params.put("vnp_OrderType", orderType);
+        // String locate = req.getParameter("language");
+        // if (locate != null && !locate.isEmpty()) {
+        // vnp_Params.put("vnp_Locale", locate);
+        // } else {
+        // vnp_Params.put("vnp_Locale", "vn");
+        // }
+        vnp_Params.put("vnp_ReturnUrl", VNPayConfig.vnp_ReturnUrl);
+
+        vnp_Params.put("vnp_IpAddr", "192.168.2.12");
+        // vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                // Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                // Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
