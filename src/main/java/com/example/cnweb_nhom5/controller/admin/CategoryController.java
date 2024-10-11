2f78b8506e50544ae55df277f0fced5c17559f47
NgocAnhPham <ngc4nh@gmail.com>
2024-10-11 16:57:16 +0700

    comment code

--- src/main/java/com/example/cnweb_nhom5/controller/admin/CategoryController.java
+++ src/main/java/com/example/cnweb_nhom5/controller/admin/CategoryController.java
@@ -24,6 +24,7 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    // Phương thức để lấy danh sách danh mục
     @GetMapping("/admin/category")
     public String getCategoryList(Model model) {
         List<Category> categories = categoryService.fetchAllCategories();
@@ -31,12 +32,14 @@ public class CategoryController {
         return "admin/category/show";
     }
 
+    // Phương thức để hiển thị trang tạo danh mục mới
     @GetMapping("/admin/category/create")
     public String getCreateCategoryPage(Model model) {
         model.addAttribute("newCategory", new Category());
         return "admin/category/create";
     }
 
+    // Phương thức để xử lý tạo danh mục mới
     @PostMapping("/admin/category/create")
     public String handleCreateCategory(
             @ModelAttribute("newCategory") @Valid Category category,
@@ -49,6 +52,7 @@ public class CategoryController {
         return "redirect:/admin/category";
     }
 
+    // Phương thức để hiển thị trang cập nhật danh mục
     @GetMapping("/admin/category/update/{id}")
     public String getUpdateCategoryPage(Model model, @PathVariable long id) {
         Optional<Category> category = categoryService.fetchCategoryById(id);
@@ -59,6 +63,7 @@ public class CategoryController {
         return "redirect:/admin/category";
     }
 
+    // Phương thức để xử lý cập nhật danh mục
     @PostMapping("/admin/category/update")
     public String handleUpdateCategory(
             @ModelAttribute("newCategory") @Valid Category category,
@@ -71,6 +76,7 @@ public class CategoryController {
         return "redirect:/admin/category";
     }
 
+    // Phương thức để xóa danh mục
     @GetMapping("/admin/category/delete/{id}")
     public String deleteCategory(@PathVariable long id) {
         categoryService.deleteCategory(id);
