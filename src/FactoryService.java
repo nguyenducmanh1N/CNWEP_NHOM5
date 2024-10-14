c470924457f43659da29803952d969c2c47818d0
VanThuyHien <vh140823@gmail.com>
2024-10-14 15:39:14 +0700

    Create FactoryService.java

---
+++ src/FactoryService.java
@@ -0,0 +1,9 @@
+package com.example.cnweb_nhom5.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.cnweb_nhom5.domain.Factory;
+import com.example.cnweb_nhom5.repository.FactoryRepository;
