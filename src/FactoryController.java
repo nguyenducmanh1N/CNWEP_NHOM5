9e1bd9b96712a4e1777713e83247480b6140770a
VanThuyHien <vh140823@gmail.com>
2024-10-14 14:26:07 +0700

    Update FactoryController.java

--- src/FactoryController.java
+++ src/FactoryController.java
@@ -14,3 +14,25 @@ import com.example.cnweb_nhom5.domain.Factory;
 import com.example.cnweb_nhom5.service.FactoryService;
 
 import jakarta.validation.Valid;
+
+@Controller
+public class FactoryController {
+
+    private final FactoryService factoryService;
+
+    public FactoryController(FactoryService factoryService) {
+        this.factoryService = factoryService;
+    }
+
+    @GetMapping("/admin/factory")
+    public String getFactoryList(Model model) {
+        List<Factory> factories = factoryService.fetchAllFactories();
+        model.addAttribute("factories", factories);
+        return "admin/factory/show";
+    }
+
+    @GetMapping("/admin/factory/create")
+    public String getCreateFactoryPage(Model model) {
+        model.addAttribute("newFactory", new Factory());
+        return "admin/factory/create";
+    }
